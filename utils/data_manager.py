64056edec5ee8411cbaffe0c0d52da079377d003
VedPeesu <vedkrp@gmail.com>
2025-02-12 16:54:00 -0800

    Loading and saving data functions

--- utils/data_manager.py
+++ utils/data_manager.py
@@ -15,3 +15,39 @@ project_analytics = {}
 file_links = {}
 team_stats = {}
 user_roles = {}
+
+def load_data():
+    """Load data from JSON file"""
+    global tasks, task_counter, reminders, notifications, project_summaries, team_members, project_analytics, file_links, team_stats, user_roles
+    try:
+        with open(DATA_FILE, 'r') as f:
+            data = json.load(f)
+            tasks = data.get('tasks', {})
+            task_counter = data.get('task_counter', 1)
+            reminders = data.get('reminders', {})
+            notifications = data.get('notifications', {})
+            project_summaries = data.get('project_summaries', {})
+            team_members = data.get('team_members', {})
+            project_analytics = data.get('project_analytics', {})
+            file_links = data.get('file_links', {})
+            team_stats = data.get('team_stats', {})
+            user_roles = data.get('user_roles', {})
+    except FileNotFoundError:
+        pass
+
+def save_data():
+    """Save data to JSON file"""
+    data = {
+        'tasks': tasks,
+        'task_counter': task_counter,
+        'reminders': reminders,
+        'notifications': notifications,
+        'project_summaries': project_summaries,
+        'team_members': team_members,
+        'project_analytics': project_analytics,
+        'file_links': file_links,
+        'team_stats': team_stats,
+        'user_roles': user_roles
+    }
+    with open(DATA_FILE, 'w') as f:
+        json.dump(data, f, indent=2, default=str)
